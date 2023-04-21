bc6a1f8f7586da7e38037edf903c28cbe60a9acb
joselbarragan <123769190+joselbarragan@users.noreply.github.com>
2023-04-21 16:29:54 -0500

    Add files via upload

---
+++ animation - copia/src/animation/Main.java
@@ -0,0 +1,18 @@
+package animation;
+
+/**
+ * @web http://jc-mouse.blogspot.com/
+ * @author Mouse
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        form1 f = new form1();
+        f.show();
+    }
+
+}
